782022d5155185970d5ddab920c3e5294b5bed94
Rohan Deshpande <rohan@creativelifeform.com>
2017-04-03 22:20:34 +1000

    Removed old implementation

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function headers() {
   });
 }
 
-function request(url, method = 'GET') {
+function fetchJson(url, method = 'GET') {
   return fetch(url, {method: method, headers: headers()})
     .then((response) => {
       if (response.ok) {
@@ -24,76 +24,12 @@ function request(url, method = 'GET') {
     });
 }
 
-// function request(url, method = 'GET', actions = {}) {
-//   function handleUnauthorized() {
-//     console.log('Refresh Token');
-//   }
-//
-//   function handleSuccess(response, actions) {
-//     return response.json().then((json) => {
-//       if (actions && typeof actions.onSuccess === 'function') {
-//         actions.onSuccess(json);
-//       }
-//     });
-//   }
-//
-//   function handleFailure(response, actions) {
-//     return response.json().then((json) => {
-//       if (actions && typeof actions.onFail === 'function') {
-//         actions.onFail(json);
-//       }
-//
-//       handleError(response.status, response.statusText);
-//     });
-//   }
-//
-//   function handleError(status, text) {
-//     let error = text;
-//
-//     if (status === 422) {
-//       error = 'Unprocessable Entity';
-//     }
-//
-//     throw new Error(error);
-//   }
-//
-//   function triage(response, actions) {
-//     if (response.ok) {
-//       return handleSuccess(response, actions);
-//     }
-//
-//     if (response.status === 401) {
-//       return handleUnauthorized();
-//     }
-//
-//     return handleFailure(response, actions);
-//   }
-//
-//   return fetch(url, { method: method, headers: headers() })
-//     .then((response) => triage(response, actions))
-//     .catch(console.error);
-// }
-
-request(`${endpoint}?code=401`, 'POST').then((json) => {
+fetchJson(`${endpoint}?code=401`, 'POST').then((json) => {
   console.log(json);
 }).catch((error) => {
   console.log(error);
 });
-// request(`${endpoint}?code=200`, 'GET', {
-//   onSuccess: function (payload) {
-//     console.log(payload);
-//   },
-//   onFail: function (payload) {
-//     console.log(payload);
-//   }
-// });
-// request(`${endpoint}?code=422`, 'GET', {
-//   onFail: function (payload) {
-//     return payload;
-//   }
-// }).then((result) => {
-//   console.log(result);
-// });
-request(`${endpoint}?code=500`).catch((error) => {
+
+fetchJson(`${endpoint}?code=500`).catch((error) => {
   console.log(error);
 });
